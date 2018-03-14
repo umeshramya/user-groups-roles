76edf877acf7094093fb50b803c631161cd936a5
Dr Umesh R Bilagi <umeshbilagi@gmail.com>
2018-03-14 14:02:26 +0530

    modified

--- index.js
+++ index.js
@@ -1,38 +1,85 @@
-var privileges =[];
-var roles = [];
+import { isUndefined } from "util";
 
-var setPrivileges = (name, description, defaultValue)=>{
+const util = require("util");
+
+var privileges =[];// holds the previlegs
+var roles = [];// holds the roles
+var rolePrivilege =[];// holds the role and concerned previlegs with value
+// if in case a perticular 
+
+var createPrivileges = (name, description, defaultValue)=>{
     // this sets the privileges
-    privileges.push = {
-        name : name,
-        description : description,
-        defualt : defaultValue
+    if(util.isUndefined(name)){
+        throw "name is required";
+    }
+    if(util.isUndefined(description)){
+        throw "Description is required";
     }
 
-
-}
-
-var getPrivileges = (privilege) => {
-    //returns the privilege
-    // search using keys in thsis arry of privileges
-}
-
-var createRole = (role, ...privilege) => {
-    // this creats the new role
-    var curPrivileges=[];
-    for (let index = 0; index < privilege.length; index++) {
-        curPrivileges[index] = privilege
+    if(util.isUndefined(defaultValue)){
+        throw "Default value is required";
     }
-    roles.push = {
-        role : curPrivileges
+    privileges.push ( [
+       name,
+       description,
+       defaultValue
+    ]);
+
+
+}
+
+
+
+var createNewRole = (role) =>{
+    // creates new role
+    roles.push(role);
+}
+
+var addPrivilegeToRole = (role, privilege, value)=>{
+    // This method sets nthe privilegs to role
+    var checked = {};//this objects holds the status of method for checking
+    for (let index = 0; index < privileges.length; index++) {
+        //check privileges for valid privileg
+        if(privileges[index][0] == name){
+            checked.privilege = true;
+            break;
+        }
+    for (let i = 0; i < roles.length; i++) {
+        // check roles for vakid role
+        if(roles[i] == role){
+            checked.role = true
+            break;
+        }
+    }
+    // if both condition satisfy then set rolePrivileg array with new prievilegs with value
+    if((checked.privilege == true) && (checked.role == true)){
+        rolePrivilege.push({
+            role   : role,
+            privilege   : privilege,
+            value   : value
+        });
+        return true
+    }else{
+        return false
     }
 
 }
 
-var getRolePrivileges = (role) => {
-    // this returns thes privileges for current role
-}
 
-var validateRole = (role) => {
-    // this is middleware 
-}
+
+
+/*
+    ====================================================
+                            TESTS
+    ====================================================
+*/ 
+createPrivileges("create", "get the secured route", false);
+
+createPrivileges("delete", "post the secured route", false);
+console.log(privileges);
+
+createNewRole("admin");
+createNewRole("editor");
+
+console.log(roles);
+
